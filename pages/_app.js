63802047573084c650688c37abf23c0f25968804
Steven Nguyen <stevenn38@hotmail.com>
2022-03-08 14:42:29 +1300

    Remove sample code and install styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    // TODO: Add theme provider
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
