372be68ace4c5a1daccb9e78aa6525771e56549d
Steven Nguyen <stevenn38@hotmail.com>
2022-03-12 02:22:18 +1300

    Create body text components and add text to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
+import styled from "styled-components";
 // Components
 import H1 from "./headings/H1";
+import Text12 from "./text/Text12";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Heading = styled(H1)`
+    margin-bottom: 1.2rem;
+`;
 
 const Header = () => {
-    return <H1>Invoices</H1>;
+    return (
+        <Container>
+            <div>
+                <Heading>Invoices</Heading>
+                <Text12>There are 0 total invoices</Text12>
+            </div>
+            <div>a;lskdjf</div>
+        </Container>
+    );
 };
 
 export default Header;
