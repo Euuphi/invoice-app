1bb45ff47f23c7c10bf4731d89a69180f818bf4a
Steven Nguyen <stevenn38@hotmail.com>
2022-03-16 15:12:52 +1300

    Configure SVGR to recognise svg URLs

--- next.config.js
+++ next.config.js
@@ -2,18 +2,26 @@
 const nextConfig = {
     reactStrictMode: true,
     webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: [
-                {
-                    loader: "@svgr/webpack",
-                    options: {
-                        icon: true,
+        config.module.rules.push(
+            {
+                test: /\.svg$/i,
+                type: "asset",
+                resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+                use: [
+                    {
+                        loader: "@svgr/webpack",
+                        options: {
+                            icon: true,
+                        },
                     },
-                },
-            ],
-        });
+                ],
+            }
+        );
 
         return config;
     },
