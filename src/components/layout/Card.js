2f718bc50f813435e4ce026c652c45f946ea1163
Steven Nguyen <stevenn38@hotmail.com>
2022-03-20 02:27:36 +1300

    Add justifyContent prop to card component

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -3,12 +3,13 @@ import styled from "styled-components";
 const Card = styled.div`
     display: flex;
     align-items: center;
+    justify-content: ${({ justifyContent }) => justifyContent};
 
     background-color: ${({ theme }) => theme.background.secondary};
     border: 1px solid transparent;
     border-radius: 0.8rem;
     box-shadow: 0 1rem 1rem ${({ theme }) => theme.shadow.light};
-    padding: 2.4rem 3.2rem;
+    padding: 2rem 3.2rem;
     width: 100%;
 `;
 
