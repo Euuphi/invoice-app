d37725d4f522b0c4a6fdb300b51c06610cbde68b
Steven Nguyen <stevenn38@hotmail.com>
2022-03-20 02:29:44 +1300

    Add flex containers to status bar element

--- src/components/invoice/InvoiceDetails/StatusBar.js
+++ src/components/invoice/InvoiceDetails/StatusBar.js
@@ -3,12 +3,16 @@ import Card from "components/layout/Card";
 // Styles
 import ParagraphMd from "components/text/ParagraphMd";
 import StatusIcon from "../../icons/StatusIcon";
+import CenteredContainer from "components/layout/CenteredContainer";
 
 const StatusBar = ({ status }) => {
     return (
-        <Card>
-            <ParagraphMd>Status</ParagraphMd>
-            <StatusIcon status={status} marginLeft="1.6rem" />
+        <Card justifyContent="space-between">
+            <CenteredContainer gap="2.4rem">
+                <ParagraphMd>Status</ParagraphMd>
+                <StatusIcon status={status} />
+            </CenteredContainer>
+            <CenteredContainer>Buttons</CenteredContainer>
         </Card>
     );
 };
