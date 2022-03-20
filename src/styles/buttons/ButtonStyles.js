37b82d168080831e280194e7efcdab2539460eab
Steven Nguyen <stevenn38@hotmail.com>
2022-03-20 21:42:40 +1300

    Create default reusable style for buttons

---
+++ src/styles/buttons/ButtonStyles.js
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+const ButtonStyles = css`
+    border: none;
+    border-radius: 100px;
+    color: #fff;
+    font-family: "Spartan", sans-serif;
+    font-size: 1.2rem;
+    font-weight: 700;
+    padding: 1.8rem 2.4rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default ButtonStyles;
