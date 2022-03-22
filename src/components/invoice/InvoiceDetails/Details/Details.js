0fba8e98c2ca6243b40cce80f174fcb6fd0bb6be
Steven Nguyen <stevenn38@hotmail.com>
2022-03-22 21:35:21 +1300

    Add client email to invoice details

--- src/components/invoice/InvoiceDetails/Details/Details.js
+++ src/components/invoice/InvoiceDetails/Details/Details.js
@@ -1,6 +1,7 @@
 import GridContainer from "components/layout/GridContainer";
 import ClientDetails from "./ClientDetails";
 import Dates from "./Dates";
+import DetailItem from "./DetailItem";
 
 const Details = ({
     createdDate,
@@ -10,17 +11,14 @@ const Details = ({
     clientEmail,
 }) => {
     return (
-        <GridContainer columns="1fr 1fr 2fr" columnGap="5rem">
+        <GridContainer columns="1fr 1fr 2fr" columnGap="5rem" width="100%">
             <Dates createdDate={createdDate} dueDate={dueDate} />
             <ClientDetails
                 name={clientName}
                 address={clientAddress}
                 email={clientEmail}
             />
-            <div>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Similique deleniti sed vitae labore?
-            </div>
+            <DetailItem title="Sent to" value={clientEmail} />
         </GridContainer>
     );
 };
