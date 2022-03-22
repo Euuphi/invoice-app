6d4762135fd92a19b3353cb9faabc8b61484eab1
Steven Nguyen <stevenn38@hotmail.com>
2022-03-22 16:39:19 +1300

    Document address component

--- src/components/invoice/InvoiceDetails/Address.js
+++ src/components/invoice/InvoiceDetails/Address.js
@@ -10,6 +10,14 @@ const AddressBox = styled.address`
     text-align: ${({ textAlign }) => textAlign}; ;
 `;
 
+/**
+ * Create <address> element to display an address object
+ *
+ * @param {object} address - Object containing address in the form of: { street, city, postCode, country }
+ * @param {string} gap - Gap between each address element
+ * @param {string} textAlign - Text align property for container element
+ * @return {JSX} <address> component displaying input object
+ */
 const Address = ({ address, gap, textAlign }) => {
     return (
         <AddressBox gap={gap} textAlign={textAlign}>
