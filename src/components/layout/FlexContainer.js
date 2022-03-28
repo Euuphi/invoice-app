8ed2888e299c747c4a90b1fab8a0acbad32878c2
Steven Nguyen <stevenn38@hotmail.com>
2022-03-28 16:59:00 +1300

    Document resuable flex container component

--- src/components/layout/FlexContainer.js
+++ src/components/layout/FlexContainer.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
+/**
+ * Create a flex container wrapper
+ *
+ * @param {string} alignItems - Set align-items property
+ * @param {string} flex - Set flex property
+ * @param {string} flexDirection - Set flex direction property
+ * @param {string} gap - Set gap property
+ * @param {string} justifyContent - Set justify-content property
+ * @param {string} width - Set width of container
+ * @return {JSX} Label and input elements
+ */
 const FlexContainer = styled.div`
     display: flex;
     align-items: ${({ alignItems }) => alignItems};
