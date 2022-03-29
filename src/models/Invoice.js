9e9bf0ef9adb7756aabaeed2c911c2a3759a27d3
Steven Nguyen <stevenn38@hotmail.com>
2022-03-30 10:55:05 +1300

    Create invoice schema model

---
+++ src/models/Invoice.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const AddressSchema = new mongoose.Schema({
+    street: String,
+    city: String,
+    postCode: String,
+    country: String,
+});
+
+const InvoiceSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: [true, "Id required"],
+        unique: true,
+    },
+    createdAt: Date,
+    paymentDue: Date,
+    description: String,
+    paymentTerms: Number,
+    clientName: String,
+    clientEmail: String,
+    status: String,
+    senderAddress: [AddressSchema],
+    clientAddress: [AddressSchema],
+    items: [
+        {
+            name: String,
+            quantity: Number,
+            price: Number,
+            total: Number,
+        },
+    ],
+    total: Number,
+});
+
+module.exports =
+    mongoose.models.Invoice || mongoose.model("Invoice", InvoiceSchema);
