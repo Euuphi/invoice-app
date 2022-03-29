8e8485a7aeda3c292d8789a1426504b24fe4a9cf
Steven Nguyen <stevenn38@hotmail.com>
2022-03-30 12:33:31 +1300

    Create api routes for invoices

---
+++ pages/api/invoices/index.js
@@ -0,0 +1,32 @@
+import dbConnect from "utils/dbConnect";
+import Invoice from "models/Invoice";
+
+dbConnect();
+
+const handler = async (req, res) => {
+    const { method } = req;
+
+    switch (method) {
+        case "GET":
+            try {
+                const invoices = await Invoice.find({});
+
+                res.status(200).json({ success: true, data: invoices });
+            } catch (err) {
+                res.status(400).json({ success: false });
+                console.log(err);
+            }
+            break;
+        case "POST":
+            try {
+                const invoice = await Invoice.create(req.body);
+
+                res.status(201).json({ success: true, data: invoice });
+            } catch (err) {
+                res.status(400).json({ success: false });
+                console.log(err);
+            }
+    }
+};
+
+export default handler;
--- pages/api/test.js
+++
@@ -1,9 +0,0 @@
-import dbConnect from "utils/dbConnect";
-
-dbConnect();
-
-async function handler(req, res) {
-    res.json({ test: "test" });
-}
-
-export default handler;
