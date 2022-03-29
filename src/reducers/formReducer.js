537da379bdb49b9203d6dca648000196409023f8
Steven Nguyen <stevenn38@hotmail.com>
2022-03-29 14:32:17 +1300

    Create redux form reducer

---
+++ src/reducers/formReducer.js
@@ -0,0 +1,16 @@
+import * as formActions from "actions/formActions";
+
+const initialState = { display: false };
+
+const form = (state = initialState, action) => {
+    switch (action.type) {
+        case formActions.CREATE_NEW_INVOICE:
+            return { ...state, display: true };
+        case formActions.DISCARD_NEW_INVOICE:
+            return { ...state, display: false };
+        default:
+            return state;
+    }
+};
+
+export default form;
