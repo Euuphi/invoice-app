dacbdd23fc671537775ad416e3c6f61ba2baa5d1
Steven Nguyen <stevenn38@hotmail.com>
2022-03-29 14:35:00 +1300

    Add form reducer to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "@reduxjs/toolkit";
 // Reducers
 import preferences from "./preferencesReducer";
 import filterOptions from "./filterOptionsReducer";
+import form from "./formReducer";
 
-export default combineReducers({ preferences, filterOptions });
+export default combineReducers({ preferences, filterOptions, form });
