f8543a08545dd47ba35966f0b744c65ce3b5a13d
Steven Nguyen <stevenn38@hotmail.com>
2022-03-31 01:19:09 +1300

    Create reusuable modal component

---
+++ src/components/ui/ConfirmationModal.js
@@ -0,0 +1,83 @@
+import styled, { css } from "styled-components";
+// Components
+import Backdrop from "components/ui/Backdrop";
+import H2 from "components/headings/H2";
+import ParagraphMd from "components/text/ParagraphMd";
+import ButtonStyle from "styles/buttons/ButtonStyle";
+import PrimaryButtonStyle from "styles/buttons/PrimaryButtonStyle";
+
+const ModalContainer = styled.div`
+    background-color: ${({ theme }) => theme.background.secondary};
+    border-radius: 0.8rem;
+    margin: auto;
+    padding: 4.8rem;
+    height: 25rem;
+    width: 48rem;
+`;
+
+const ConfirmButton = styled.button`
+    ${({ customStyle }) => customStyle};
+`;
+
+const CancelButton = styled.button`
+    ${({ customStyle }) => customStyle};
+`;
+
+/**
+ * Modal for action confirmation with backdrop
+ *
+ * @param {function} cancelOnClick - Click handler for cancel action
+ * @param {function} confirmOnClick - Click handler for confirm action
+ * @param {css} cancelButtonStyle - Styled-component css custom cancel button style
+ * @param {css} confirmButtonStyle - Styled-component css Custom confirm button style
+ * @param {string} confirmButtonText - Text for confirm button. Default - "Confirm"
+ * @param {string} title - Modal title text
+ * @param {string} body - Modal paragraph text
+ * @return {JSX}
+ */
+const ConfirmationModal = ({
+    cancelOnClick,
+    confirmOnClick,
+    cancelButtonStyle,
+    confirmButtonStyle,
+    confirmButtonText,
+    title,
+    body,
+}) => {
+    return (
+        <Backdrop>
+            <ModalContainer>
+                <H2>{title}</H2>
+                <ParagraphMd>{body}</ParagraphMd>
+                <CancelButton
+                    onClick={cancelOnClick}
+                    customStyle={cancelButtonStyle}>
+                    Cancel
+                </CancelButton>
+                <ConfirmButton
+                    onClick={confirmOnClick}
+                    customStyle={confirmButtonStyle}>
+                    {confirmButtonText || "Confirm"}
+                </ConfirmButton>
+            </ModalContainer>
+        </Backdrop>
+    );
+};
+
+export default ConfirmationModal;
+
+const CancelButtonStyle = css`
+    ${ButtonStyle}
+
+    background-color: ${({ theme }) => theme.button.light.background.default};
+    color: ${({ theme }) => theme.button.light.text.default};
+`;
+
+const ConfirmButtonStyle = css`
+    ${PrimaryButtonStyle}
+`;
+
+ConfirmationModal.defaultProps = {
+    cancelButtonStyle: CancelButtonStyle,
+    confirmButtonStyle: ConfirmButtonStyle,
+};
