6b951f70b1c438f2ee462e6b3b7a7adba32a742d
Steven Nguyen <stevenn38@hotmail.com>
2022-04-03 22:00:32 +1300

    Create selector for getting form item by id

--- src/stores/selectors/formInputSelectors.js
+++ src/stores/selectors/formInputSelectors.js
@@ -4,3 +4,9 @@ export const getFormInputs = createSelector(
     (state) => state.formInput,
     (formInputs) => formInputs
 );
+
+export const getItemById = createSelector(
+    (state) => state.formInput.items,
+    (state, id) => id,
+    (items, id) => items.filter((item) => item.id === id)[0]
+);
