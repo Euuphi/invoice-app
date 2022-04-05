ed034997af7a58d93faa55df1a07b07fe9533c5b
Steven Nguyen <stevenn38@hotmail.com>
2022-04-06 11:47:05 +1200

    Update form due date when date inputs are changed

--- src/components/forms/InvoiceForm/InvoiceFormDetails.js
+++ src/components/forms/InvoiceForm/InvoiceFormDetails.js
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateInput } from "stores/actions/formInputActions";
+import addDays from "functions/addDays";
 import InputField from "../InputField";
 import FormGroup from "../FormGroup";
 import SelectField from "../SelectField";
 
 const InvoiceFormDetails = () => {
+    const dispatch = useDispatch();
+    // Selectors
+    const paymentTerm = useSelector((state) => state.formInput.paymentTerms);
+    const dateCreated = useSelector((state) => state.formInput.createdAt);
+
+    // Update payment due date when there are changes to payment date inputs
+    useEffect(() => {
+        // Error handler
+        if (!dateCreated || !paymentTerm) {
+            return;
+        } else {
+            // Calculate due day with created date + payment term
+            const paymentDue = addDays(
+                dateCreated,
+                Number(paymentTerm)
+            ).toISOString();
+
+            // Update payment due date within form state
+            dispatch(updateInput("paymentDue", paymentDue));
+            return paymentDue;
+        }
+    }, [dateCreated, paymentTerm, dispatch]);
+
     return (
         <FormGroup columns="1fr 1fr">
             <InputField
