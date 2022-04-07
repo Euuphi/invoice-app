96a631361efb4ed8d6d402f7e1c8c46394ab9c5b
Steven Nguyen <stevenn38@hotmail.com>
2022-04-07 15:31:15 +1200

    Add function to submit edited invoice

--- src/components/forms/InvoiceForm/EditInvoiceFormButtons.js
+++ src/components/forms/InvoiceForm/EditInvoiceFormButtons.js
@@ -1,21 +1,42 @@
-import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
 import { hideForm } from "stores/actions/formActions";
 import { pageScrollOn } from "stores/actions/uiActions";
 // Components
 import FormButtonsContainer from "components/layout/FormButtonsContainer";
 import SaveButton from "components/buttons/formButtons/SaveButton";
 import CancelButton from "components/buttons/formButtons/CancelButton";
+import { getFormInputs } from "stores/selectors/formInputSelectors";
 
 const EditInvoiceFormButton = () => {
     const dispatch = useDispatch();
+    const router = useRouter();
+
+    const { invoiceId } = router.query;
+    const formInputs = useSelector((state) => getFormInputs(state));
 
     const cancelHandler = () => {
         dispatch(hideForm());
         dispatch(pageScrollOn());
     };
 
-    const saveHandler = () => {
-        console.log("Save");
+    const saveHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(
+                `http://localhost:3000/api/invoices/${invoiceId}`,
+                {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(formInputs),
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+
+        router.reload(window.location.pathname);
     };
 
     return (
