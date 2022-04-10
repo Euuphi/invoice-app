ac7085bdf016400b4b9e3e8ba0db6d3090487ab1
Steven Nguyen <stevenn38@hotmail.com>
2022-04-11 08:55:30 +1200

    Add input validation to edit form

--- src/components/forms/InvoiceForm/EditInvoiceFormButtons.js
+++ src/components/forms/InvoiceForm/EditInvoiceFormButtons.js
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import { FormContext } from "context/FormContext";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
-import { hideForm } from "stores/actions/formActions";
+import {
+    hideForm,
+    setInputErrors,
+    setSubmitting,
+} from "stores/actions/formActions";
 import { pageScrollOn } from "stores/actions/uiActions";
 // Components
 import FormButtonsContainer from "components/layout/FormButtonsContainer";
@@ -11,6 +17,7 @@ import { getFormInputs } from "stores/selectors/formInputSelectors";
 const EditInvoiceFormButton = () => {
     const dispatch = useDispatch();
     const router = useRouter();
+    const { validateInputs, validateErrors } = useContext(FormContext);
 
     const { invoiceId } = router.query;
     const formInputs = useSelector((state) => getFormInputs(state));
@@ -22,21 +29,29 @@ const EditInvoiceFormButton = () => {
 
     const saveHandler = async (e) => {
         e.preventDefault();
+        dispatch(setSubmitting(true));
 
-        try {
-            const response = await fetch(
-                `http://localhost:3000/api/invoices/${invoiceId}`,
-                {
-                    method: "PUT",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(formInputs),
-                }
-            );
-        } catch (error) {
-            console.log(error);
+        // Validate input fields
+        const inputErrors = validateInputs(formInputs);
+        const errors = validateErrors(inputErrors);
+        dispatch(setInputErrors(inputErrors));
+
+        if (Object.keys(errors).length === 0) {
+            try {
+                const response = await fetch(
+                    `http://localhost:3000/api/invoices/${invoiceId}`,
+                    {
+                        method: "PUT",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify(formInputs),
+                    }
+                );
+            } catch (error) {
+                console.log(error);
+            }
+
+            router.reload(window.location.pathname);
         }
-
-        router.reload(window.location.pathname);
     };
 
     return (
