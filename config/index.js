780c994fd8699f9e8749d2010aab4ca6475c8712
Steven Nguyen <stevenn38@hotmail.com>
2022-04-13 23:50:07 +1200

    Add correct production URL path

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const BASE_URL = dev
     ? "http://localhost:3000"
-    : "https://kaleidoscopic-piroshki-9af0f5";
+    : "https://kaleidoscopic-piroshki-9af0f5.netlify.app/";
