7aca76006356ca045ffb3ad3b9cfa634cdc8c1a4
Steven Nguyen <stevenn38@hotmail.com>
2022-04-14 17:27:18 +1200

    Fix flash of unstyled content (FOUC)

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,45 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="icon" href="/favicon-32x32.png" />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+export default class MyDocument extends Document {
+    static async getInitialProps(context) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = context.renderPage;
+
+        try {
+            context.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(context);
+
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="icon" href="/favicon-32x32.png" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
