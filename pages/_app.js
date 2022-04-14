b23a68b6e983b04854be7f597764464bad248023
Steven Nguyen <stevenn38@hotmail.com>
2022-04-14 13:36:33 +1200

    Import font from global css file instead

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-// Styled Component
-import { createGlobalStyle } from "styled-components";
+import "./global.css";
 // Redux
 import { Provider as ReduxProvider } from "react-redux";
 import store from "stores/store";
@@ -9,29 +8,10 @@ import DarkThemeProvider from "themes/providers/DarkThemeProvider";
 import Background from "components/layout/Background";
 import Nav from "components/nav/Nav";
 
-const GlobalStyle = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
-
-    html {
-        font-size: 62.5%;
-        overflow-y: scroll;
-    }
-
-    body {
-        font-family: "Spartan", sans-serif;
-    }
-
-`;
-
 function MyApp({ Component, pageProps }) {
     return (
         <ReduxProvider store={store}>
             <DarkThemeProvider>
-                <GlobalStyle />
                 <Nav />
                 <Background>
                     <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -5,17 +5,6 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="icon" href="/favicon-32x32.png" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Spartan:wght@500;600;700&display=swap"
-                    rel="stylesheet"
-                />
             </Head>
             <body>
                 <Main />
