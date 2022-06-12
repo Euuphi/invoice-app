522bc62f47dd550fc8380073131dfe96f242a056
Steven Nguyen <stevenn38@hotmail.com>
2022-06-12 18:56:59 +1200

    Add adjustable gap property to FormGroup

--- src/components/forms/FormGroup.js
+++ src/components/forms/FormGroup.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const FormGroup = styled.fieldset`
     display: grid;
-    gap: 2.4rem;
+    gap: ${({ gap }) => gap || "2.4rem"};
     grid-template-columns: ${({ columns }) => columns};
     margin-bottom: 4.8rem;
 
